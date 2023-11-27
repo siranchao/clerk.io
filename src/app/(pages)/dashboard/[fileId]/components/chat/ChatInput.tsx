ec533563d88d59f3c5b0054735305743035002f1
Siran Cao <siranchao@gmail.com>
2023-11-27 14:37:46 -0500

    input chat textarea

--- src/app/(pages)/dashboard/[fileId]/components/chat/ChatInput.tsx
+++ src/app/(pages)/dashboard/[fileId]/components/chat/ChatInput.tsx
@@ -1,4 +1,6 @@
-
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button"
+import { Send } from "lucide-react"
 
 interface ChatInputProps {
     isDisabled?: boolean
@@ -6,13 +8,29 @@ interface ChatInputProps {
 
 export function ChatInput( { isDisabled }: ChatInputProps ) {
 
-
-
-
-
     return (
         <>
-        
+            <div className="absolute bottom-0 left-0 w-full">
+                <form className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+                    <div className="relative flex h-full flex-1 items-stretch md:flex-col">
+                        <div className="relative flex flex-col w-full flex-grow p-4">
+                            <div className="relative">
+                                <Textarea 
+                                    rows={1} 
+                                    maxRows={4} 
+                                    placeholder="Enter your question..." 
+                                    autoFocus
+                                    className="resize-none pr-12 text-base py-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
+                                />
+
+                                <Button className="absolute bottom-1.5 right-[8px]" aria-label="Send message">
+                                    <Send className="h-4 w-4"/>
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </>
     )
 }
