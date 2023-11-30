155cc205b8711317e481da29d88c27a9cc98fb0b
Siran Cao <siranchao@gmail.com>
2023-11-30 14:49:48 -0500

    setup stripe & payment process

---
+++ src/config/stripe.ts
@@ -0,0 +1,32 @@
+
+
+export const PLANS = [
+    {
+        name: 'Free',
+        slug: 'free',
+        quota: 10,
+        pageLimit: 5,
+        price: {
+            amount: 0,
+            priceIds: {
+                test: "",
+                production: ""
+            }
+        }
+    },
+    {
+        name: 'Pro',
+        slug: 'pro',
+        quota: 50,
+        pageLimit: 25,
+        price: {
+            amount: 12,
+            priceIds: {
+                test: "price_1OICnDHGJt2vcyXx9rq2silm",
+                production: ""
+            }
+        }
+    },
+]
+
+
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function absoluteUrl(path: string) {
+  if(typeof window !== 'undefined') {
+    return window.location.origin + path
+  }
+
+  if(process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}${path}`
+  }
+
+  return `http://localhost:${process.env.PORT ?? 3000}${path}`
+}
