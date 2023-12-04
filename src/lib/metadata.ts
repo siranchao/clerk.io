6f76b763dfe15a1e4fe3942c95d841d4432890ae
Siran Cao <siranchao@gmail.com>
2023-12-04 15:41:32 -0500

    add metadata

---
+++ src/lib/metadata.ts
@@ -0,0 +1,45 @@
+import type { Metadata } from 'next'
+
+export function constructMetadata({
+    title = "clerk.io - the AI assistant helps you chat with your PDFs",
+    description = "clerk.io is an open-source software to use AI and LLM to process and analyze PDF files. It allows you to chat with AI based on your document contexts.",
+    image = "/thumbnail.png",
+    icons = "/favicon.ico",
+    noIndex = false
+  }: {
+    title?: string
+    description?: string
+    image?: string
+    icons?: string
+    noIndex?: boolean
+  } = {}): Metadata {
+    return {
+      title,
+      description,
+      openGraph: {
+        title,
+        description,
+        images: [
+          {
+            url: image
+          }
+        ]
+      },
+      twitter: {
+        card: "summary_large_image",
+        title,
+        description,
+        images: [image],
+        creator: "@ChaoSiran"
+      },
+      icons,
+      metadataBase: new URL(process.env.APP_URL || 'http://localhost:3000'),
+      themeColor: '#FFF',
+      ...(noIndex && {
+        robots: {
+          index: false,
+          follow: false
+        }
+      })
+    }
+  }
